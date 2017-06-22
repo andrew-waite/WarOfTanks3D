56f31cc90cbfda59ddf62edc59c2b362a12a909a
Andrew Waite <drewrockz01@gmail.com>
2017-06-22 11:55:58 +1000

    Added shooting of prefabs & meshes based on rotation

---
+++ Assets/Scripts/ProjectileFlight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileFlight : MonoBehaviour {
+
+	public GameObject turretTransform;
+	private GameObject cannonball;
+	// Use this for initialization
+	void Start () {
+		this.cannonball = Resources.Load ("Sphere") as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.T)) {
+			//GameObject ball = Instantiate (cannonball) as GameObject;
+			//ball.transform.Rotate(new Vector3(0,90,0));
+			//ball.transform.position = turretTransform.transform.position;
+			//ball.GetComponent<Rigidbody> ().velocity = (turretTransform.transform.rotation * Vector3.forward) * 20;
+
+
+			GameObject ball = Instantiate (cannonball, this.turretTransform.transform.position, this.turretTransform.transform.rotation) as GameObject;
+			ball.GetComponent<Rigidbody> ().AddForce (1000 * -this.turretTransform.transform.right);
+			Destroy (ball, 2);
+		}
+	}
+}
---
+++ Assets/Scripts/TurretRotation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretRotation : MonoBehaviour {
+
+	private float zRotation = 0.0F;
+	private float yRotation = 90.0F;
+	private Transform transformation;
+
+	// Use this for initialization
+	void Start () {
+		this.transformation = this.gameObject.GetComponent<Transform> ();
+		this.zRotation = this.transformation.rotation.z;
+		this.yRotation = this.transformation.rotation.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.G)) {
+			if (zRotation > -180)
+				this.gameObject.GetComponent<Transform> ().Rotate (new Vector3 (0, 0, this.zRotation -= 2));
+			else {
+			}
+		} else if (Input.GetKeyUp (KeyCode.B)) {
+			if (zRotation < 0)
+				this.gameObject.GetComponent<Transform> ().Rotate (new Vector3 (0, 0, this.zRotation += 2));
+			else {
+			}	
+		} else if (Input.GetKeyUp (KeyCode.F)) {
+			this.gameObject.GetComponent<Transform> ().Rotate (new Vector3 (0, yRotation -= 2, 0));
+		} else if (Input.GetKeyUp (KeyCode.H)) {
+			this.gameObject.GetComponent<Transform> ().Rotate (new Vector3 (0, yRotation += 2, 0));
+		}
+
+		this.zRotation = this.transformation.rotation.z;
+		this.yRotation = this.transformation.rotation.y;
+	}
+}
